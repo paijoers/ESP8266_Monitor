5e7df5cd01e3b164b237fbb093035e6edbe6a78d
NYS PJR <74069017+paijoers@users.noreply.github.com>
2023-07-23 19:16:52 +0700

    esp8266_monitor.ino

--- esp8266_monitor/esp8266_monitor.ino
+++ esp8266_monitor/esp8266_monitor.ino
@@ -9,6 +9,7 @@ Author  : NyessId
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
@@ -541,7 +542,7 @@ void setup(){
 void loop(){
   server.handleClient();
   if(WiFi.status() != WL_CONNECTED){
-    if(millis() - statusMillis >= 60000) {
+    if(millis() - statusMillis >= 120000) {
       statusMillis = millis(); 
       Serial.println("Wi-Fi Error!, menghubungkan kembali..");
       lcd.clear();
